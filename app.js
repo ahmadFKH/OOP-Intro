975e4088055abc74e327bbdceb34d0c3fffec723
Ahmad Khalilieh <ahmadfk90@gmail.com>
2018-04-30 14:45:07 +0300

    ex 3

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 class Animal {
-    constructor(name, weight, energyLevel) {
-        this.name = name;
-        this.weight = weight;
-        this.energyLevel = energyLevel;
-        this.foodAmount = 10; 
-
+    get name() {
+        return this._name;
     }
+
+    set name(newName) {
+        if (newName.length > 2) {
+            this._name = newName;
+        }
+       
+        else {
+            console.log("Error: provided name must be 2 characters or longer");
+        }
+    }
+    get weight() {
+        return this._weight;
+    }
+
+    set weight(newWeight) {
+        if (newWeight > 0) {
+            this._weight = newWeight;
+        }
+       
+        else {
+            console.log("Error: provided weight must be above zero");
+        }
+    }
+    constructor(name, weight) {
+        this._name = name;
+        this._weight = weight;
+    }
+
     greet() {
         console.log("Hello, I'm " + this.name);
-      }
-    play() {
+    }
+}
+    /*play() {
         this.energyLevel -= 50; 
         console.log("Now my energy level = " + this.energyLevel);
         return;
@@ -25,12 +50,26 @@ class Elephant extends Animal {
         super(name, weight,energyLevel);
         this.trunkLength = trunkLength;
     }
-}
+}*/
 class Fish extends Animal {
-    constructor(name, weight, energyLevel, foodAmount) {
-        super(name, weight,energyLevel);
-        this.foodAmount = 2;
+    constructor(name, weight) {
+        super(name, weight);
     }
+    get weight() {
+        debugger;
+        console.log("ANIMAL_NAME weight accessed");
+        return this._weight;
+    }
+    set weight(newWeight) {
+        if (newWeight <= 100) {
+            this._weight = newWeight;
+        }  
+        else {
+            console.log("Error: provided weight must be below 100");
+        }
+}
+}
+/*
     play() {
         this.energyLevel -= 5; 
         console.log("Now my energy level = " + this.energyLevel);
@@ -58,9 +97,13 @@ class Lion extends Mammals {
 
 let thomas = new Lion("Thomas", 3000,10000);
 console.log(thomas);
-/*let billy = new Animal('Billy', 500,100);
+let billy = new Animal('Billy', 500,100);
 let jeffrey = new Animal("Jeffrey", 100);
 console.log("Hello " + billy.name);
 console.log(billy.greet());
 billy.play();
 billy.eat();*/
+let billy = new Fish('Billy', 500);
+console.log("Hello " + billy.name);
+billy.name = 'ah';
+billy.weight = 101;
